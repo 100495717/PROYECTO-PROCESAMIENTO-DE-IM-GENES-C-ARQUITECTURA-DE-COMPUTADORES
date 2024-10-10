ae062b37ba82b584e71b90e9b9997c9da8623f1e
100495992 <100495992@alumnos.uc3m.es>
2024-10-10 19:33:03 +0200

    Co-authored-by: 100495925 <100495925@users.noreply.github.com>

--- imtool-aos/main.cpp
+++ imtool-aos/main.cpp
@@ -18,40 +18,37 @@ int main(int argc, char* argv[]){
         if (operation == "info"){
             print_image_info(img);
         } 
+
         //Si la operación es "maxlevel" aplicamos el nivel máximo
-        else if (operation == "maxlevel"){
-            if (args.size() < 5){
-                throw std::invalid_argument("Falta el argumento de nivel máximo");
-            }
+        else if (operation == "maxlevel") {
             int maxlevel = std::stoi(args[4]);
             max_level(img, maxlevel);
-        } 
+        }
+
         //Si la operación es "resize" redimensionamos la imagen
-        else if (operation == "resize"){
-            if (args.size() != 6){
-                throw std::invalid_argument("Faltan los argumentos de ancho y alto");
-            }
+        else if (operation == "resize") {
             int width = std::stoi(args[4]);
             int height = std::stoi(args[5]);
             resize_image(img, width, height);
 
         }
+
         //Si la operación es "cutfreq" cortamos la frecuencia 
         else if (operation == "cutfreq"){
-            if (args.size() != 5){
-                throw std::invalid_argument("Falta el argumento de frecuencia de corte");
-            }
             int n = std::stoi(args[4]);
             cut_least_freq(img, n);
-        } else if (operation == "compress"){
+        } 
+        
+        //Si la operación es "cutfreq" hacemos una conversión de la imagen al formato cppm
+        else if (operation == "compress"){
             compress_image(img, output);
-        } else {
-            throw std::invalid_argument("Operación no válida" + operation);
-        }
+        } 
 
         //Escribimos la imagen modificada en la salida
         write_ppm(output, img);
-    }catch (const std::exception& e){
+    }
+    
+    catch (const std::exception& e){
         std::cerr << "Error: " << e.what() << std::endl;
         return 1;
     }
--- src/common/progargs.cpp
+++ src/common/progargs.cpp
@@ -4,11 +4,120 @@
 
 //parse_arguments convierte los argumentos de argv[] en un vector de cadenas (std::vector<std::string>)
 std::vector<std::string> parse_arguments(int argc, char*argv[]){
-    // Si hay menos de 3 argumentos lanzamos la excepción
+    // Si hay menos de 4 argumentos lanzamos la excepción
     if (argc < 4){
-        throw std::invalid_argument("Los argumentos deben ser: imtool <entrada.ppm> <salida.ppm> <operacion>");
+        throw std::invalid_argument("Error: Invalid number of arguments: " + std::to_string(argc - 1));
     }
-    return std::vector<std::string>(argv, argv+argc);
+    
+    std::vector<std::string> args(argv, argv+argc);
+
+    std::string operation = args[3];
+
+    // Validamos los argumentos para la operación "info"
+    if (operation == "info"){
+            if (argc != 4){
+                throw std::invalid_argument("Error: Invalid extra arguments for info: " + args[4]);
+            }
+            // CASO DE QUE SI SE CONTEMPLEN N ARGUMENTOS ERRONEOS
+            /*
+                std::string extra_args = "";
+                for (int i = 4; i < argc; ++i) {
+                    extra_args += args[i] + " ";
+                }
+                throw std::invalid_argument("Error: Invalid extra arguments for info: " + extra_args);
+            */
+
+    } 
+
+    //// Validamos los argumentos para la operación "maxlevel"
+    else if (operation == "maxlevel"){
+        //Comprobamos que el número de argumentos es el correcto, sino devolvemos error          
+        if(argc!=5){
+            throw std::invalid_argument("Error: Invalid number of arguments for maxlevel: " + std::to_string(argc-4));
+        }
+        
+        //Pasamos maxsize de string a entero
+        try{
+             int maxlevel = std::stoi(args[4]);
+
+        //Comprobamos que el valor de maxsize esta entre los valores permitidos
+        if(0>maxlevel>65535){
+            throw std::invalid_argument("Error: Invalid maxlevel: " + std::to_string(maxlevel));
+        }
+        }
+
+        catch(const std::invalid_argument& e){
+             ("Error: Invalid maxlevel" + args[4]);
+        }   
+    } 
+    
+    // Validdamos los argumentos para la operación "resize"
+    else if (operation == "resize"){
+        // Comprobamos primero que el número de argumentos sea corrrecto
+        if (argc != 6) {
+            throw std::invalid_argument("Error: Invalid number of extra arguments for resize: " + std::to_string(argc - 4));
+        }
+        
+        // Ahora validamos que los datos introducidos sean correctos
+        int width = std::stoi(args[4]);
+        int height = std::stoi(argv[5]);
+
+        if (width <= 0){
+            throw std::invalid_argument("Error: Invalid resize width: " + args[4]);
+        }
+        if (height <= 0){
+            throw std::invalid_argument("Error: Invalid resize height: " + args[5]);
+        }
+        
+    }
+
+    // Validamos los argumentos para la operación "cutfreq"
+    else if (operation == "cutfreq"){
+        //Comprobamos que el número de argumentos es el correcto, sino devolvemos error          
+        if(argc!=5){
+            throw std::invalid_argument("Error: Invalid number of arguments for maxlevel: " + std::to_string(argc-4));
+        }
+        
+        //Pasamos maxsize de string a entero
+        try{
+             int cutfreq = std::stoi(args[4]);
+
+        //Comprobamos que el valor de maxsize esta entre los valores permitidos
+        if(0>cutfreq){
+            throw std::invalid_argument("Error: Invalid cutfreq: " + std::to_string(cutfreq));
+        }
+        }
+
+        catch(const std::invalid_argument& e){
+             ("Error: Invalid cutfreq" + args[4]);
+        }   
+    } 
+    
+
+    // Validamos los argumentos para la operación "compress"
+    else if (operation == "compress"){
+       if (argc != 4){
+                throw std::invalid_argument("Error: Invalid extra arguments for compress: " + args[4]);
+            }
+            // CASO DE QUE SI SE CONTEMPLEN N ARGUMENTOS ERRONEOS
+            /*
+                std::string extra_args = "";
+                for (int i = 4; i < argc; ++i) {
+                    extra_args += args[i] + " ";
+                }
+                throw std::invalid_argument("Error: Invalid extra arguments for compress: " + extra_args);
+            */
+    } 
+    
+    // Si la operación no es ninguna de las anteriores no es válida
+    else {
+        throw std::invalid_argument("Error: Invalid option: " + operation);
+    }
+
+
+
+
+
 }
 
 
