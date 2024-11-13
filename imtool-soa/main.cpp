a0b3cd4d0cdd8cb4bb928cab1985741db605b12a
100499744 <158488890+100499744@users.noreply.github.com>
2024-11-14 00:49:11 +0100

    main soa cambiado

--- imtool-soa/main.cpp
+++ imtool-soa/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
-#include "imgaos/ppm.hpp"
-#include "common/progars.hpp"
-#include "imgaos/ppm.hpp"
+#include "../common/progargs.hpp"
+#include "../imgaos/ppm-aos.hpp"
+#include "../imgaos/image-operations-aos.hpp"
+#include "../common/binaryio.hpp"
+#include <stdexcept>
+#include <vector>
+#include <string>
+
 
 int main(int argc, char* argv[]){
     try{
@@ -9,51 +14,54 @@ int main(int argc, char* argv[]){
         std:: string input = args[1];
         std:: string output = args[2];
         std:: string operation = args[3];
-    
-    
-        // Leemos la imagen
-        ImageAos img = read_ppm(input);
+
+        // Leer la imagen
+        ImageAos img = read_ppm_image_aos(input);
 
         // Hacer la operación
-        // Si la operación es "info" imprimimos la información de la imagen
-        if (operation == "info"){
-            print_image_info(img);
-        } 
-        // Si la operación es "maxlevel" aplicamos el nivel máximo
-        else if (operation == "maxlevel"){
-            if (args.size() < 5){
-                throw std::invalid_argument("Falta el argumento de nivel máximo");
-            }
-            int maxlevel = std::stoi(args[4]);
-            max_level(img, maxlevel);
-        } 
-        // Si la operación es "resize" redimensionamos la imagen
-        else if (operation == "resize"){
-            if (args.size() != 6){
-                throw std::invalid_argument("Faltan los argumentos de ancho y alto");
-            }
-            int width = std::stoi(args[4]);
-            int height = std::stoi(args[5]);
-            resize_image(img, width, height);
-
-        }
-        // Si la operación es "cutfreq" cortamos la frecuencia 
-        else if (operation == "cutfreq"){
-            if (args.size() != 5){
-                throw std::invalid_argument("Falta el argumento de frecuencia de corte");
-            }
-            int n = std::stoi(args[4]);
-            cut_least_freq(img, n);
-        } else if (operation == "compress"){
+        if (operation == "compress"){
+        
             compress_image(img, output);
-        } else {
-            throw std::invalid_argument("Operación no válida" + operation);
-        }
+        } 
+        else{
+            // Si la operación es "info" imprimimos la información de la imagen
+            if (operation == "info"){
+                print_image_info(img);
+            } 
 
-        // Escribimos la imagen modificada en la salida
-        write_ppm(output, img);
-    }catch (const std::exception& e){
-        std::cerr << "Error: " << e.what() << std::endl;
+            // Si la operación es "maxlevel" aplicamos el nivel máximo
+            else if (operation == "maxlevel") {
+                int maxlevel = std::stoi(args[4]);
+                max_level(img, maxlevel);
+            }
+
+            // Si la operación es "resize" redimensionamos la imagen
+            else if (operation == "resize") {
+                int width = std::stoi(args[4]);
+                int height = std::stoi(args[5]);
+                resize_image(img, width, height);
+            }
+
+            // Si la operación es "cutfreq" cortamos la frecuencia 
+            else if (operation == "cutfreq"){
+                int n = std::stoi(args[4]);
+                cut_freq(img, n);
+            } 
+            else if (operation == "write"){
+                write_ppm_image_aos(output, img);
+            }
+            else{
+                throw std::runtime_error("Operación no válida");
+            }
+        
+
+            // Escribimos la imagen modificada en la salida
+            write_ppm_image_aos(output, img);
+        }
+    }
+    
+    catch (const std::exception& e){
+        std::cerr << "" << e.what() << std::endl;
         return 1;
     }
     return 0;
